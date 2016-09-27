05407f6205bbef7828306eff09e153b8e5d045ed
root <root@localhost>
2016-09-27 10:19:52 -0400

    debian installer

---
+++ debian_installer.sh
@@ -0,0 +1,16 @@
+echo '''
+Debian installer
+'''
+echo 'installing packages'
+apt-get install git libgmp-dev python-pip python-dev curl libcurl4-openssl-dev libssl-dev libffi libpth-dev libffi-dev python-cffi libsodium-dev
+
+echo 'update'
+
+apt-get update && apt-get upgrade
+
+echo 'python setup'
+pip install fte regex2dfa scrypt
+python setup.py build
+python setup.py install
+echo 'now you just have to configure it :)'
+
