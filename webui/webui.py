b0019fff40b7482b76940dc149ab124ac697b457
root <root@localhost.localdomain>
2016-11-26 11:30:31 +0100

    anti fingerprinting

--- webui/webui.py
+++ webui/webui.py
@@ -1,44 +1,87 @@
 import flask
-import gnupg
+import gnupg, base64
 #https://gist.github.com/dustismo/6203329 / apt-get install libleveldb1 libleveldb-dev && pip install plyvel
 #import plyvel #leveldb, very fast, you can even run the database in ram if you want
 #import MySQLdb #if you want mysql
 from os import urandom
 from base64 import b64decode
 import datetime
+import sys
+from functools import wraps
 
 
+#datab = marionette_tg.conf.get("server.database")
+datab = 'mysql'
+
+if datab == 'leveldb':
+	import plyvel
+elif datab == 'mysql':
+    	import MySQLdb 
+else:
+	print 'error'
 #webui for layerprox 
-#beta version, dont use until stable version
 
 lp = flask.Flask(__name__) 
 
 dbplace = '' #database directory, test: '/tmp/testdb/'
 
+def add_response_headers(headers={}):
+	def decorator(f):
+		@wraps(f)
+		def decorated_function(*args, **kwargs):
+			resp = flask.make_response(f(*args, **kwargs))
+			h = resp.headers
+			for header, value in headers.items():
+				h[header] = value
+			return resp
+		return decorated_function
+	return decorator
+
+
+
 #general trolling bots
 @lp.before_request
 def blockuseragentreq():
-    useragent = flask.request.headers['User-Agent']
-    if 'sqlmap' in useragent:
-        return flask.redirect(flask.url_for('dummy'))
-    elif 'wget' in useragent:
-        return flask.redirect(flask.url_for('dummy'))
-    elif 'w3af' in useragent:
-        return flask.redirect(flask.url_for('dummy'))
-    elif 'curl' in useragent:
-        return flask.redirect(flask.url_for('dummy'))
-    elif 'Scanner' in useragent:
-        return flask.redirect(flask.url_for('dummy'))
+	useragent = flask.request.headers['User-Agent']
+	if 'sqlmap' in useragent:
+		return flask.redirect(flask.url_for('dummy'))
+	elif 'wget' in useragent:
+		return flask.redirect(flask.url_for('dummy'))
+	elif 'w3af' in useragent:
+		return flask.redirect(flask.url_for('dummy'))
+#	elif 'curl' in useragent:
+#		return flask.redirect(flask.url_for('dummy'))
+	elif 'Scanner' in useragent:
+		return flask.redirect(flask.url_for('dummy'))
         
-    else:
-        pass
+	else:
+		pass
 
+
+#root@box:~# curl -I http://127.0.0.1:80/
+#HTTP/1.0 200 OK
+#Content-Type: text/html; charset=utf-8
+#Content-Length: 198
+#Server: amiIN9vf36G1T3xzpg==
+#Date: Sat, 26 Nov 2016 10:24:22 GMT
+
+
+
+#protection against server fingerprinting 
+def antifingerprint(f):
+	jibbrish = base64.b64encode(urandom(13))
+	@wraps(f)	
+	@add_response_headers({'Server': jibbrish})
+	def decorated_function(*args, **kwargs):
+		return f(*args, **kwargs)
+	return decorated_function
 
 
 
 @lp.route('/')
+@antifingerprint
 def firstpage():
-    return '''
+	return '''
 
 <html>
 <head>
@@ -66,16 +109,14 @@ def firstpage():
 
 @lp.route('/getproxied', methods=['GET', 'POST'])
 def get_registerd():
-    if flask.request.method == 'POST':
-        day = gen_day()
-        ckey = flask.form.request('pgp')
-        gpg.import_keys(ckey)
-        h1 = urandom()
-        fingerprint = 'x'
-        return '''
+	if flask.request.method == 'POST':
+		day = gen_day()
+		h1 = urandom()
+		fingerprint = 'x'
+		return '''
 <html>      
 <head>
-<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+
 <title> LayerProx</title>
    
    </head>
@@ -88,41 +129,34 @@ def get_registerd():
 
       </html>  
         '''    
+
     
-    
-    
-    
-    
-    
-    return '''
-    <html>      
-    <head>
-    <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
-    <title> LayerProx</title>
-       
-       </head>
-    <center>
-            <body>
-            Enter Your Public pgp key<br>
-            <form method='POST' action=''>
-            <textarea name='pgp'></textarea>
-            <br><input type='submit' value='import'>            
-            </form>
-            
-            </body>
-    
-    
-          </html>  
-            '''    
+#choice a serve
+#choice a serverr
+
+#if sucess redirect to
+@lp.route('/welcome')
+def wel():
+	return ''' 
+<html>
+
+<body>
+<center>
+<h1>Welcome to the LayerProx network</h1>
+</body>
+
+</html>
+	'''
+
 #make the bots read 1984
 @lp.route('/nobots')
 def dummy():
-    return flask.redirect('http://msxnet.org/orwell/1984.pdf', code=302)
+	return flask.redirect('http://msxnet.org/orwell/1984.pdf', code=302)
 
 #tell noone pub info, only dax
 @lp.route('/robots.txt')
 def robots():
-    return '''
+	return '''
     User-agent: *
     Dissallow: /
     
@@ -133,16 +167,16 @@ def robots():
     '''
 
 def check_db():
-    db = plyvel.DB(dbplace, create_if_missing=True)
+	db = plyvel.DB(dbplace, create_if_missing=True)
     #db.put(b'20', b'value')
-    today = datetime.date.today()
-    today = str(today)
+        today = datetime.date.today()
+        today = str(today)
     #key prefix is date + hmac, one value is date
-    for key, value in db.iterator(start=today):
-        if key:
-            db.delete(key)
-        else:
-            pass
+        for key, value in db.iterator(start=today):
+		if key:
+			db.delete(key)
+		else:
+			pass
 
 
 
@@ -158,18 +192,18 @@ def check_db():
 
 #day generation system
 def gen_day():
-    test = map(ord, urandom(10))
-    test = str(test[0])# first number pair
-    test = test[0] # first number
-    test = int(test[0])
-    test2 = map(ord, urandom(test))
+	test = map(ord, urandom(10))
+	test = str(test[0])# first number pair
+	test = test[0] # first number
+	test = int(test[0])
+	test2 = map(ord, urandom(test))
 
-    number = test2[test] 
-    today = datetime.date.today()
-    number = int(number)
+	number = test2[test] 
+	today = datetime.date.today()
+	number = int(number)
     #number * daytime
-    day = today + datetime.timedelta(days=+number) #plus int generated 
-    return day
+        day = today + datetime.timedelta(days=+number) #plus int generated 
+	return day
 
 
 #mysql - db 
@@ -181,4 +215,4 @@ def gen_day():
 
 
 if __name__ == '__main__':
-    lp.run(debug=False,host='0.0.0.0',port=80) #host=0.0.0.0 
+	lp.run(debug=False,port=80) #host=0.0.0.0 
