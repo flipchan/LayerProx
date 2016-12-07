edc5cf39983f44b02a6fd399c95174802f7e61d5
flipchan <flipchan@users.noreply.github.com>
2016-12-07 15:36:21 +0100

    Update ubuntu_installer.sh

--- ubuntu_installer.sh
+++ ubuntu_installer.sh
@@ -2,7 +2,7 @@ echo 'u need to run this as root'
 
 echo 'installing packages'
 apt-get install python2.7 libgmp-dev python-pip python-dev libcurl4-openssl-dev libssl-dev libpth-dev python-cffi libffi-dev 
-pip install fte regex2dfa gnupg flask
+pip install fte regex2dfa gnupg flask pyblake2
 echo 'i recommend setting up a testuser with gnupg so the homedir = /home/testuser/.gnupg'
 apt-get update && apt-get upgrade
 echo 'more packages'
